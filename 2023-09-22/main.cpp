010b5985efd06159e322ea867844d1d94fbedef4
Yihsiang Liow <yliow@ccis.edu>
2023-09-25 14:44:36 -0400

    no msg

---
+++ 2023-09-22/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // int x = 2;
+    // double y = 3.1;
+    // std::cout << double(x) + y << '\n';
+
+    double x;
+    std::cin >> x;
+    int i = int(x + 0.5);
+    std::cout << i << '\n';
+    return 0;
+}
