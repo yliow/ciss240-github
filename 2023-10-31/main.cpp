acf88eaca3e5a342464f312337f8ed623ddaff98
Yihsiang Liow <yliow@ccis.edu>
2023-10-31 16:06:47 -0400

    no msg

--- 2023-10-31/main.cpp
+++ 2023-10-31/main.cpp
@@ -13,6 +13,26 @@ bool is_prime(int n)
     return true;
 }
 
+int two()
+{
+    return 2;
+}
+
+int square(int x)
+{
+    return x * x;
+}
+
+int factorial(int n)
+{
+    int p = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        p *= i;
+    }
+    return p;
+}
+
 int main()
 {
     // int count = 0;
@@ -29,13 +49,21 @@ int main()
     std::cin >> n;
     for (int p1 = 2; p1 <= n / 2; ++p1)
     {
-        for (int p2 = 2; p2 <= n / 2; ++p2)
+        int p2 = n - p1;
+        if (is_prime(p1) && is_prime(p2))
         {
-            if (n == p1 + p2 && isprime(p1) && isprime(p2))
-            {
-                std::cout << p1 << ' ' << p2 << '\n';
-            }
+            std::cout << p1 << ' ' << p2 << '\n';
         }
+    }
+
+    std::cout << two() << '\n';
+    for (int x = 0; x < 10; ++x)
+    {
+        std::cout << x << ' ' << square(x) << '\n';
+    }
+    for (int x = 0; x < 20; ++x)
+    {
+        std::cout << x << ' ' << factorial(x) << '\n';
     }
     
     return 0;
