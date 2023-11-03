59caba537d8ad87ae8069768bb09c7bb98ddb048
Yihsiang Liow <yliow@ccis.edu>
2023-11-03 15:07:01 -0400

    no msg

--- 2023-11-03/diamond/main.cpp
+++ 2023-11-03/diamond/main.cpp
@@ -6,7 +6,39 @@ int main()
     std::cin >> n;
     for (int i = n - 1; i >= 0; --i)
     {
-        std::cout << i << ' ' << 2 * n - 3 - 2 * i << '\n';
+        for (int j = 0; j < i; ++j)
+        {
+            std::cout << ' ';
+        }
+        std::cout << (i == n - 1 || i == 0 ? '+' : '/');
+        if (2 * n - 3 - 2 * i > 0)
+        {
+            for (int j = 0; j < 2 * n - 3 - 2 * i; ++j)
+            {
+                std::cout << (j % 2 == 0 ? ' ' : '*');
+            }
+            std::cout << '\\';
+        }
+        std::cout << '\n';
     }
+
+    for (int i = 1; i <= n - 1; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            std::cout << ' ';
+        }
+        std::cout << '\\';
+        if (2 * n - 3 - 2 * i > 0)
+        {
+            for (int j = 0; j < 2 * n - 3 - 2 * i; ++j)
+            {
+                std::cout << (j % 2 == 0 ? ' ' : '*');
+            }
+            std::cout << '/';
+        }
+        std::cout << '\n';
+    }
+
     return 0;
 }
