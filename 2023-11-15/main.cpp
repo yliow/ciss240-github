b8499195f94881d1eeef4193dc22fa28ccd88fce
Yihsiang Liow <yliow@ccis.edu>
2023-11-15 15:24:11 -0500

    no msg

--- 2023-11-15/main.cpp
+++ 2023-11-15/main.cpp
@@ -4,51 +4,81 @@
 
 int main()
 {
-    srand(0);
-    const int n = 10;
+    srand(4);
+    const int n = 1000;
     int x[n];
 
     for (int i = 0; i < n; ++i)
     {
         x[i] = rand() % n;
     }
-    for (int i = 0; i < n; ++i)
-    {
-        std::cout << x[i] << ' ' ;
-    }
-    std::cout << '\n';
-
-    int target = rand() % n;
-    // std::cout << target << '\n';
-    
-    // linear search
-    int index = -1;
-    for (int i = 0; i < n; ++i)
-    {
-        if (x[i] == target)
-        {
-            index = i;
-            break;
-        }
-    }
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     std::cout << x[i] << ' ' ;
+    // }
+    // std::cout << '\n';
 
     // bubblesort
     std::cout << "bubblesort ...\n";
-    for (int i = n - 2; i >= 0; --i))
+    for (int i = n - 2; i >= 0; --i)
     {
         for (int j = 0; j <= i; ++j)
         {
             if (x[j] > x[j + 1])
+                {
+                    int t = x[j];
+                    x[j] = x[j + 1];
+                    x[j + 1] = t;
+                }
+            // for (int i = 0; i < n; ++i)
+            // {
+            //     std::cout << x[i] << ' ' ;
+            // }
+            // std::cout << '\n';
+        }
+    }
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     std::cout << x[i] << ' ' ;
+    // }
+    // std::cout << '\n';
+
+    for (int test = 0; test < 20; ++test)
+    {
+        int target = rand() % n;
+        // std::cout << target << '\n';
+        
+        // linear search
+        // int index = -1;
+        // for (int i = 0; i < n; ++i)
+        // {
+        //     if (x[i] == target)
+        //     {
+        //         index = i;
+        //         break;
+        //     }
+        // }
+
+        //binary search
+        int index = -1;  
+        int left = 0, right = n - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            //std::cout << left << ' ' << mid << ' ' << right << '\n';
+            if (x[mid] == target)
             {
-                int t = x[j];
-                x[j] = x[j + 1];
-                x[j + 1] = x[j];
+                index = mid;
+                break;
             }
-            for (int i = 0; i < n; ++i)
+            else if (target < x[mid])
             {
-                std::cout << x[i] << ' ' ;
+                right = mid - 1;
             }
-            std::cout << '\n';
+            else
+            {
+                left = mid + 1;
+            }
         }
     }
     return 0;
