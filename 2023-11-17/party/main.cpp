bf83f11d6e8c3b09a19dd8414cfd90362b00008b
Yihsiang Liow <yliow@ccis.edu>
2023-11-27 14:33:51 -0500

    no msg

---
+++ 2023-11-17/party/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int addr[1024];
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> addr[i];
+    }
+
+    int min_tot_dist = 10000;
+    int best_addr;
+
+    for (int i = 0; i < n, ++i)
+    {
+        // compute total dist walking to addr[i]
+        int tot_dist = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            tot_dist += fabs(addr[j] - addr[i]);
+        }
+        // now compute running best ...
+        if (i == 0)
+        {
+            min_tot_dist = tot_dist;
+            best_addr = 0;
+        }
+        else
+        {
+            if (tot_dist < min_tot_dist)
+            {
+                min_tot_dist = tot_dist;
+                best_addr = i;
+            }
+        }
+    }
+    
+    return 0;
+}
