099f8a70bb0938b992b8391aed3b4770dfe00ebc
Yihsiang Liow <yliow@ccis.edu>
2023-12-04 15:22:44 -0500

    no msg

---
+++ 2023-12-04/chess/chess.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+void init_board(char chessboard[8][8]);
+void print_board(char chessboard[8][8]);
+ 
+int main()
+{
+    char chessboard[8][8];
+    init_board(chessboard);
+    print_board(chessboard);
+    
+    return 0;
+}
+
+void init_board(char chessboard[8][8])
+{
+    for (int c = 0; c < 8; ++c)
+    {
+        chessboard[1][c] = 'p';
+        chessboard[6][c] = 'P';
+        for (int r = 2; r < 6; ++r)
+        {
+            chessboard[r][c] = ' ';
+        }
+    }
+    char pieces[9] = "RNBQKBNR";
+    for (int c = 0; c < 8; ++c)
+    {
+        chessboard[7][c] = pieces[c];
+        chessboard[0][c] = pieces[c] - 'A' + 'a';
+    }
+    
+}
+
+void print_board(char chessboard[8][8])
+{
+    std::cout << "+-+-+-+-+-+-+-+-+\n";
+    for (int r = 0; r < 8; ++r)
+    {
+        std::cout << '|';
+        for (int c = 0; c < 8; ++c)
+        {
+            std::cout << chessboard[r][c] << '|';
+        }
+        std::cout << '\n';
+        std::cout << "+-+-+-+-+-+-+-+-+\n";
+    }
+}
