b5c9868a6ca90846d0d8a8c25023fef4154cee9f
Yihsiang Liow <yliow@ccis.edu>
2023-12-06 15:22:11 -0500

    no msg

--- 2023-12-06/main.cpp
+++ 2023-12-06/main.cpp
@@ -43,13 +43,30 @@ int main()
 {
     char s[NROWS][NCOLS];
     int spaceship_r = NROWS - 1, spaceship_c = NCOLS / 2;
-    int alien_r = 1, alien_c = NCOLS / 2;
+    int alien_r = 1, alien_c = NCOLS / 2, alien_isalive = true;
+    int alien_dc = -1;
     int laser_r = 0, laser_c = 0, laser_isalive = false;
+    int score = 0;
     while (1)
     {
         clear(s);
         s[spaceship_r][spaceship_c] = 'A';
-        s[alien_r][alien_c] = 'V';
+        s[0][0] = score + '0';
+        if (alien_isalive)
+        {
+            alien_c += alien_dc;
+            if (alien_c < 0)
+            {
+                alien_c = 0;
+                alien_dc = -alien_dc;
+            }
+            else if (alien_c >= NCOLS)
+            {
+                alien_c = NCOLS - 1;
+                alien_dc = -alien_dc;
+            }            
+            s[alien_r][alien_c] = 'V';
+        }
         if (laser_isalive)
         {
             s[laser_r][laser_c] = '|';
@@ -59,6 +76,15 @@ int main()
         if (laser_isalive)
         {
             --laser_r;
+            if (laser_r < 0)
+            {
+                laser_isalive = false;
+            }
+            if (laser_r == alien_r && laser_c == alien_c)
+            {
+                alien_isalive = false;
+                ++score;
+            }
         }
         std::cout << "l-left r-right f-fire n-no input q-quit: ";
         char input;
