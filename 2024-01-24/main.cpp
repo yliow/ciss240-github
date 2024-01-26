f16eb0d2d1307519906774bcb64bc508321c2594
Yihsiang Liow <yliow@ccis.edu>
2024-01-26 14:57:34 -0500

    no msg

---
+++ 2024-01-24/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    std::cin >> x;
+    std::cin >> y;
+    std::cin >> z;
+    std::cout << x + y * z << '\n';
+    return 0;
+}
