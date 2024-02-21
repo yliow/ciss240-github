325875a1dd73dc9d881b320b3a6c9c657e1f7855
Yihsiang Liow <yliow@ccis.edu>
2024-02-21 14:57:30 -0500

    no msg

--- 2024-02-21/main.cpp
+++ 2024-02-21/main.cpp
@@ -2,28 +2,55 @@
 
 int main()
 {
+    // int x0 = 1, x1  = 3, x2 = 0, x3 = 5;
+    // int max;
+    
+    // max = x0;
+    // std::cout << x0 << ' ' << max << '\n';
+    
+    // if (x1 > max)
+    // {
+    //     max = x1;
+    // }
+    // std::cout << x1 << ' ' << max << '\n';
+    
+    // if (x2 > max)
+    // {
+    //     max = x2;
+    // }
+    // std::cout << x2 << ' ' << max << '\n';
+
+    // if (x3 > max)
+    // {
+    //     max = x3;
+    // }
+    // std::cout << x3 << ' ' << max << '\n';
+
+    // running minimum computation
+    
     int x0 = 1, x1  = 3, x2 = 0, x3 = 5;
+    int min;
+    
+    min = x0;
+    std::cout << x0 << ' ' << min << '\n';
+    
+    if (x1 < min)
+    {
+        min = x1;
+    }
+    std::cout << x1 << ' ' << min << '\n';
+    
+    if (x2 < min)
+    {
+        min = x2;
+    }
+    std::cout << x2 << ' ' << min << '\n';
 
-    max = x0;
-    std::cout << x0 << ' ' << max << '\n';
-    
-    if (x1 > max)
+    if (x3 < min)
     {
-        max = x1;
+        min = x3;
     }
-    std::cout << x1 << ' ' << max << '\n';
-    
-    if (x2 > max)
-    {
-        max = x2;
-    }
-    std::cout << x2 << ' ' << max << '\n';
+    std::cout << x3 << ' ' << min << '\n';
 
-    if (x3 > max)
-    {
-        max = x3;
-    }
-    std::cout << x3 << ' ' << max << '\n';
-    
     return 0;
 }
