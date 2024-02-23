a83c2ce6d0cf78ad2c001d9e4bc158d4ab6a2a1c
Yihsiang Liow <yliow@ccis.edu>
2024-02-23 14:31:50 -0500

    no msg

--- 2024-02-21/main.cpp
+++ 2024-02-21/main.cpp
@@ -28,29 +28,63 @@ int main()
 
     // running minimum computation
     
-    int x0 = 1, x1  = 3, x2 = 0, x3 = 5;
-    int min;
+    // int x0 = 1, x1  = 3, x2 = 0, x3 = 5;
+    // int min;
     
-    min = x0;
-    std::cout << x0 << ' ' << min << '\n';
+    // min = x0;
+    // std::cout << x0 << ' ' << min << '\n';
     
-    if (x1 < min)
-    {
-        min = x1;
-    }
-    std::cout << x1 << ' ' << min << '\n';
+    // if (x1 < min)
+    // {
+    //     min = x1;
+    // }
+    // std::cout << x1 << ' ' << min << '\n';
     
-    if (x2 < min)
-    {
-        min = x2;
-    }
-    std::cout << x2 << ' ' << min << '\n';
+    // if (x2 < min)
+    // {
+    //     min = x2;
+    // }
+    // std::cout << x2 << ' ' << min << '\n';
 
-    if (x3 < min)
-    {
-        min = x3;
-    }
-    std::cout << x3 << ' ' << min << '\n';
+    // if (x3 < min)
+    // {
+    //     min = x3;
+    // }
+    // std::cout << x3 << ' ' << min << '\n';
 
+    int x0 = 0, x1 = 3, x2 = 0, x3 = 1, x4 = 3;
+    int target = 42;
+    int count = 0;
+
+    if (x0 == target)
+    {
+        count = count + 1;
+    }
+    std::cout << x0 << ' ' << count << '\n';
+
+    if (x1 == target)
+    {
+        count = count + 1;
+    }
+    std::cout << x1 << ' ' << count << '\n';
+
+    if (x2 == target)
+    {
+        count = count + 1;
+    }
+    std::cout << x2 << ' ' << count << '\n';
+
+    if (x3 == target)
+    {
+        count = count + 1;
+    }
+    std::cout << x3 << ' ' << count << '\n';
+
+    if (x4 == target)
+    {
+        count = count + 1;
+    }
+    std::cout << x4 << ' ' << count << '\n';
+    
     return 0;
 }
