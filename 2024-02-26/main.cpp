fa4986cd99618e5fb193e08eb40c35d46958a710
Yihsiang Liow <yliow@ccis.edu>
2024-02-27 15:01:52 -0500

    no msg

---
+++ 2024-02-26/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+int main()
+{
+    // int x = 5;
+    // int r = (x < 5 ? 42 : 0);
+    // std::cout << r << '\n';
+
+    // if (x < 5)
+    // {
+    //     r = 42;
+    // }
+    // else
+    // {
+    //     r = 0;
+    // }
+    // std::cout << r << '\n';
+
+    // int x;
+    // std::cin >> x;
+
+    // switch (x)
+    // {
+    //     case 1:
+    //         std::cout << "yo" << '\n';
+    //         break;
+
+    //     case 2:
+            
+    //     case 3:
+    //         std::cout << "oh?\n";
+    //         //break;
+          
+    //     default:
+    //         std::cout << "huh?\n";
+    //         break;
+    // }
+
+    // int j;
+    
+    // int i = 1;
+    // std::cout << i << '\n';
+    // j = (i++);
+    // std::cout << i << ' ' << j << '\n';
+
+    // i = 1;
+    // std::cout << i << '\n';
+    // j = (++i);
+    // std::cout << i << ' ' << j << '\n';
+
+    // i = 1;
+    // --i;
+    // std::cout << i << '\n';
+    // i = 1;
+    // i--;
+    // std::cout << i << '\n';
+
+    // i = 2;
+    // i += 5;
+    // std::cout << i << '\n';
+
+    // i = 2;
+    // i -= 3;
+    // std::cout << i << '\n';
+
+    // i = 2;
+    // i *= 3;
+    // std::cout << i << '\n';
+
+
+    std::cout << "before\n";
+
+    for (int i = 0; i < 10; i = i + 1)
+    {
+        std::cout << i << '\n';
+    }
+    
+    std::cout << "after\n";
+
+    return 0;
+}
