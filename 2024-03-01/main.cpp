9b912b8b8d0356b7c72b8d1bca612ca21d7003b0
Yihsiang Liow <yliow@ccis.edu>
2024-03-01 14:43:50 -0500

    no msg

---
+++ 2024-03-01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // double s = 0.0;
+    // for (int i = 1; i <= 100000; ++i)
+    // {
+    //     s = s + 1.0 / i;
+    //     std::cout << i << ' ' << ' ' << s << '\n';
+    // }
+    // std::cout << s << '\n';
+
+    return 0;
+}
