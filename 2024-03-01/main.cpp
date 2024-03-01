0f1f1fd56ab1552241868b211af5b1991d8c15ed
Yihsiang Liow <yliow@ccis.edu>
2024-03-01 15:04:33 -0500

    no msg

--- 2024-03-01/main.cpp
+++ 2024-03-01/main.cpp
@@ -10,5 +10,42 @@ int main()
     // }
     // std::cout << s << '\n';
 
+    // int prod = 1;
+    // for (int i = 1; i <= 20; ++i)
+    // {
+    //     int term = i;
+    //     prod = prod * term;
+    //     std::cout << i << ' ' << term << ' ' << prod << '\n';
+    // }
+    // std::cout << prod << '\n';
+
+    /*
+      p = 1
+      i = 1
+      i = 1:
+          term = 1
+          p = p * term = 1 * 1
+      i = 2:
+          term = 2
+          p = p * term = 1 * 1 * 2
+      i = 3:
+          term = 3
+          p = p * term = 1 * 1 * 2 * 3
+     */
+
+    /*
+      n = 8
+      1 * 3 * 5 * 7
+     */
+    // int n;
+    // std::cin >> n;
+    // int p = 1;
+    // for (int i = 1; i < n; i += 2)
+    // {
+    //     p *= i;
+    //     std::cout << i << ' ' << p << '\n';
+    // }
+    // std::cout << p << '\n';
+    
     return 0;
 }
