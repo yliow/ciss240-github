cc91bdd92c8c14cb3f131f3e0d0594d62ebf2054
Yihsiang Liow <yliow@ccis.edu>
2024-03-06 14:37:07 -0500

    no msg

--- 2024-03-04/main.cpp
+++ 2024-03-04/main.cpp
@@ -25,19 +25,29 @@ int main()
     // std::cout << min << '\n';
 
     // generating equally spaced points
-    double a, b;
-    int n;
-    std::cin >> a >> b >> n;
+    // double a, b;
+    // int n;
+    // std::cin >> a >> b >> n;
 
-    double x = a;
-    double dx = (b - a) / (n - 1);
-    for (int i = 0; i < n; ++i)
-    {
-        std::cout << x << '\n';
+    // double x = a;
+    // double dx = (b - a) / (n - 1);
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     std::cout << x << '\n';
 
-        // for next iteration
-        x += dx;
-    }
+    //     // for next iteration
+    //     x += dx;
+    // }
+
+    // another way
+    // double a, b; // end points of interval
+    // int n;       // number of points
+    // std::cin >> a >> b >> n;
+    // double dx = (b - a) / (n - 1); // length of a subinterval
+    // for (double x = a; x <= b; x = x + dx)
+    // {
+    //     std::cout << x << '\n';
+    // }
     
     return 0;
 }
