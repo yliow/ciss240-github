ea8aaf5d5b4bc0b0e2ceb22aa7f3107819907161
Yihsiang Liow <yliow@ccis.edu>
2024-03-11 14:55:08 -0400

    no msg

---
+++ 2024-03-11/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    // int n;
+    // std::cin >> n;
+
+    // for (int i = 1; i <= n; ++i)
+    // {
+    //     // print i stars
+    //     for (int j = 0; j < i; ++j)
+    //     {
+    //         std::cout << '*';
+    //     }
+    //     // print newline
+    //     std::cout << '\n';
+    // }
+
+    return 0;
+}
