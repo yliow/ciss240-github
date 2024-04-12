cc0052f94360663316e31b9410daa6d5001b0c7b
Yihsiang Liow <yliow@ccis.edu>
2024-04-12 15:20:38 -0400

    no msg

--- 2024-04-12/main.cpp
+++ 2024-04-12/main.cpp
@@ -6,7 +6,7 @@ double max(double, double);
 void zero_out(int & i);
 void swap(int &, int &);
 void set_zero(int x[], int n);
-
+void swap(int x[], int i, int j);
 int main()
 {
     // printDollar(123);
@@ -17,21 +17,36 @@ int main()
     // std::cout << max(5.01, 1.25) << '\n';
     // std::cout << max(12.34, 12.34) << '\n';
 
-    int i = 42;
-    zero_out(i); // intention: set i to 0.
-    std::cout << i << '\n';
+    // int i = 42;
+    // zero_out(i); // intention: set i to 0.
+    // std::cout << i << '\n';
 
-    int x = 5, y = 123;
-    swap(x, y);
-    std::cout << x << ' ' << y << '\n';
+    // int x = 5, y = 123;
+    // swap(x, y);
+    // std::cout << x << ' ' << y << '\n';
 
-    int z[10] = {5, 3, 1, 2, 4, 6, 7, 2, 3, 1};
-    set_zero(z, 5);
-    for (int i = 0; i < 10; ++i)
-    {
-        std::cout << z[i] << ' ';
-    }
-    std::cout << '\n';
+    // int z[10] = {5, 3, 1, 2, 4, 6, 7, 2, 3, 1};
+    // set_zero(z, 5);
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     std::cout << z[i] << ' ';
+    // }
+    // std::cout << '\n';
+
+    // swap(z, 5, 8);
+    // for (int i = 0; i < 10; ++i)
+    // {
+    //     std::cout << z[i] << ' ';
+    // }
+    // std::cout << '\n';
+
+    int w[3][4];
+    w[1][2] = 42;
+    std::cout << w[i][2] << '\n';
+
+
+
+    
     return 0;
 }
 
@@ -76,4 +91,12 @@ void set_zero(int x[], int n)
     {
         x[i] = 0;
     }
+}
+
+// swap x[i] and x[j]
+void swap(int x[], int i, int j)
+{
+    int t = x[i];
+    x[i] = x[j];
+    x[j] = t;
 }
