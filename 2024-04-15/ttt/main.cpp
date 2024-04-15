70d57c1a7a5e1121834354822ccf1dacd044da64
Yihsiang Liow <yliow@ccis.edu>
2024-04-15 15:22:55 -0400

    no msg

--- 2024-04-15/ttt/main.cpp
+++ 2024-04-15/ttt/main.cpp
@@ -4,20 +4,36 @@ const int n = 3;
 
 void init_board(char board[n][n]);
 void print_board(char board[n][n]);
-
+void get_move(char board[n][n], char turn, int & row, int & col);
 
 int main()
 {
     char board[n][n];
     init_board(board);
-    
     board[0][1] = 'X';
     board[1][2] = '?';
 
- 
-    
+    char turn = 'X';
+    while (1)
+    {
+        
+        // Get move from player. (note: keep asking for row, col until it's valid.)
+        int row, col;
+        get_move(board, turn, row, col);
+        
+        // make move for player
+        board[row][col] = turn;
+
+        // check if game ended, i.e., 
+        // 1. there's a winner
+        // 2. it's a draw ... every empty square has been taken
+        
+        // toggle the turn variable
+        turn = (turn == 'X' ? 'O' : 'X');
+    }
     return 0;
 }
+
 
 void print_board(char board[n][n])
 {
@@ -59,6 +75,23 @@ void init_board(char board[n][n])
         {
             board[row][col] = ' ';
         }
+    }
+    return;
+}
+
+
+void get_move(char board[n][n], char turn, int & row, int & col)
+{
+    while (1)
+    {
+        print_board(board);
+        std::cout << "player " << turn << " ... enter move with row and col: ";
+        std::cin >> row >> col;
+        if (board[row][col] == ' ')
+        {
+            break;
+        }
+        std::cout << "WRONG MOVE!!!\n";
     }
     return;
 }
