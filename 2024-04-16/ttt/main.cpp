075b68b2c9b40a9f47ca912b1c64fe9351a6953b
Yihsiang Liow <yliow@ccis.edu>
2024-04-16 15:30:45 -0400

    no msg

--- 2024-04-16/ttt/main.cpp
+++ 2024-04-16/ttt/main.cpp
@@ -6,6 +6,7 @@ void init_board(char board[n][n]);
 void print_board(char board[n][n]);
 void get_move(char board[n][n], char turn, int & row, int & col);
 bool is_winning_move(char board[n][n], int row, int col);
+bool is_winning_row(char board[n][n], int row, char piece);
 
 int main()
 {
@@ -101,4 +102,39 @@ void get_move(char board[n][n], char turn, int & row, int & col)
     return;
 }
 
-bool is_winning_move(char board[n][n], int row, int col);
+// Check if row row is a winning
+bool is_winning_row(char board[n][n], int row, char piece)
+{
+    for (int c = 0; c < n; ++c)
+    {
+        if (board[row][c] != piece)
+        {
+            win = false;
+            break;
+        }
+    }
+    if (win) return true;
+}
+
+bool is_winning_move(char board[n][n], int row, int col)
+{
+    char piece = board[row][col]; // the piece that the player just put down
+    bool win = true;
+    
+    
+    // check if column col is a winning col
+    win = true;
+    for (int r = 0; r < n; ++r)
+    {
+        if (board[r][col] != piece)
+        {
+            win = false;
+            break;
+        }
+    }
+    if (win) return true;
+    
+    // check if forward diag is winning diag    
+    // check if reverse diag is winning diag    
+
+}
