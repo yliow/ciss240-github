e111d81031da97057bcf9b97fd7d4ad4392f6fb5
Yihsiang Liow <yliow@ccis.edu>
2024-04-17 15:10:23 -0400

    no msg

---
+++ 2024-04-17/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+// void swap(int & a, int & b)
+// {
+//     int t = a;
+//     a = b;
+//     b = t;
+// }
+// void bubblesort(int x[], int n)
+// {
+//     for (int i = 0; i < n - 2; ++i)
+//     {
+//         for (int j = 0; j <= i; ++j)
+//         {
+//             if (x[j] > x[j + 1])
+//             {
+//                 swap(x[j], x[j + 1]);
+//             }    
+//         }
+//     }
+// }
+
+// int count(int x[], int n, int target)
+// {
+//     int counter = 0;
+//     for (int i = 0; i < n; ++i)
+//     {
+//         if (x[i] == target)
+//         {
+//             ++counter;
+//         }
+//     }
+//     return counter;
+// }
+
+// bool isequal(int x[], int y[], int start, int end)
+// {
+//     for (int i = start; i < end; ++i)
+//     {
+//         if (x[i] != y[i])
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+const int n = 10;
+
+int main()
+{
+
+    // int x[1000] = {5, 3, 1, 2, 4, 6};
+    // bubblesort(x, 6);
+
+    // int x[3] = {4, 2, 5};
+    // int y[3] = {4, 2, 5};
+    // std::cout << isequal(x, y, 0, 3) << '\n';
+    // this does not compare array contents!!!!!!!
+    //std::cout << (x == y) << '\n';
+    //std::cout << x << '\n';
+    //std::cout << y << '\n';
+    //std::cout << (x != y) << '\n';
+
+    int ship_row = 9;
+    int ship_col = n / 2;
+
+    char surface[n][n];
+    clear(surface);
+    draw(surface);
+
+    return 0;
+}
+
+void clear(char surface[n][n])
+{
+}
