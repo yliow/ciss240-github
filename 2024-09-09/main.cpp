f496fddde95c7e424ae9e49fb59582a60eb697eb
Yihsiang Liow <yliow@ccis.edu>
2024-09-10 15:06:49 -0400

    no msg

---
+++ 2024-09-09/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 6 << " x " << 7 << " = " << 42 << "\n";
+    std::cout << 5 / 3 << '\n';
+    //std::cout << 5.1 / 3.2 << '\n';
+
+    std::cout << 9 % 3 << '\n';
+    std::cout << 10 % 3 << '\n';
+    std::cout << 11 % 3 << '\n';
+    std::cout << 12 % 3 << '\n';
+    std::cout << 13 % 3 << '\n';
+    std::cout << 14 % 3 << '\n';
+    return 0;
+}
