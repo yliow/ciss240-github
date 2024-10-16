fb0643b4bfb26fed90c3a6ae5525c198b0f78b62
Yihsiang Liow <yliow@ccis.edu>
2024-10-16 14:35:10 -0400

    no msg

---
+++ 2024-10-16/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    // int start, end;
+    // std::cin >> start >> end;
+    
+    // std::cout << std::setw(8) << "n"
+    //           << std::setw(8) << "n^2" << '\n';
+    // std::cout << std::setw(8) << "---"
+    //           << std::setw(8) << "---" << '\n';
+    // for (int i = start; i < end + 1; ++i)
+    // {
+    //     std::cout << std::setw(8) << i
+    //               << std::setw(8) << i * i << '\n';
+    // }
+    return 0;
+}
