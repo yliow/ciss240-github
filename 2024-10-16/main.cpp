69652c8c7fe8dee06596e2a46a6effed913a97a2
Yihsiang Liow <yliow@ccis.edu>
2024-10-21 14:34:07 -0400

    no msg

--- 2024-10-16/main.cpp
+++ 2024-10-16/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 int main()
 {
@@ -15,11 +16,49 @@ int main()
     //     std::cout << std::setw(8) << i
     //               << std::setw(8) << i * i << '\n';
     // }
-
-    for (double x = 3.1; x < 7.2; x += 0.1)
-    {
-        std::cout << x << '\n';
-    }
     
+    // const double PI = 3.14159;
+    // for (double x = 0.0; x < PI / 2; x += 0.1)
+    // {
+    //     std::cout << x << ' ' << sin(x) << '\n';
+    // }
+
+    // std::cout << x << '\n';
+    // int x = 5;
+
+    // for (int i = 0; i < 3; ++i)
+    // {
+    //     std::cout << i << '\n';
+    // }
+    // std::cout << "i???" << i << '\n';
+
+    // int s = 0;
+
+    // for (int i = 1; i < 6; ++i)
+    // {
+    //     std::cout << '\n';
+    //     std::cout << "top ..." << i << ' ' << s << '\n';
+    //     s = s + i;
+    //     std::cout << "bottom ..." << i << ' ' << s << '\n';
+
+    // }
+    // std::cout << "final s:" << s << '\n';
+
+
+    // sum of 1*1 + ... + 5*5
+    // int s = 0;
+    // for (int i = 1; i < 6; ++i)
+    // {
+    //     s += i * i;
+    //     std::cout << i << ' ' << s << '\n';
+    // }
+    // std::cout << s << '\n'; 
+
+    double s = 0;
+    for (int i = 1; i < 6; ++i)
+    {
+        s += 1.0 / (i * i);
+        std::cout << i << ' ' << 1.0 / (i * i) << ' ' << s << '\n';
+    }
     return 0;
 }
