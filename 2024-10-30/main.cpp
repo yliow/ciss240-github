dbfe588d5a38c3b3368c9f68f43b740397ade0cf
Yihsiang Liow <yliow@ccis.edu>
2024-10-30 14:53:05 -0400

    no msg

--- 2024-10-30/main.cpp
+++ 2024-10-30/main.cpp
@@ -2,15 +2,29 @@
 
 int main()
 {
-    for (int i = 3; i < 6; ++i)
-    {
-        std::cout << i << '\n';
-        for (int j = i; j <= 2 * i; ++j)
-        {
-            std::cout << ' ' << j;
-        }
-        std::cout << '\n';
-    }
+    // for (int i = 3; i < 6; ++i)
+    // {
+    //     std::cout << i << '\n';
+    //     for (int j = i; j <= 2 * i; ++j)
+    //     {
+    //         std::cout << ' ' << j;
+    //     }
+    //     std::cout << '\n';
+    // }
 
+    // draw n spaces then 1 star
+    /*
+    int n;
+    std::cin >> n;
+
+    // draw n spaces
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << ' ';
+    }
+    std::cout << '*';
+    std::cout << '\n';
+    */
+    
     return 0;
 }
