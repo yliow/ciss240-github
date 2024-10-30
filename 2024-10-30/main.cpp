f2dd9c2858c6850b9f8de95aa9b423f77e8b05c2
Yihsiang Liow <yliow@ccis.edu>
2024-10-30 15:14:19 -0400

    no msg

--- 2024-10-30/main.cpp
+++ 2024-10-30/main.cpp
@@ -25,6 +25,35 @@ int main()
     std::cout << '*';
     std::cout << '\n';
     */
+
+    /* draw rect
+    int w, h;
+    std::cin >> w >> h;
+    
+    // draw box of 3x5
+    for (int j = 0; j < h; ++j)
+    {
+        // draw line of width 5
+        for (int i = 0; i < w; ++i)
+        {
+            std::cout << '*';
+        }
+        std::cout << '\n';
+    }
+    */
+
+    // draw rt angle triangle, rt angle bottom left
+    int n;
+    std::cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        // draw i stars
+        for (int j = 0; j < i; ++j)
+        {
+            std::cout << '*';
+        }
+        std::cout << '\n';
+    }
     
     return 0;
 }
