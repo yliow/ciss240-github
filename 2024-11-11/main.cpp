40e1a8a44b5cb534c113b144a541f59823b49789
Yihsiang Liow <yliow@ccis.edu>
2024-11-11 15:03:55 -0500

    no msg

--- 2024-11-11/main.cpp
+++ 2024-11-11/main.cpp
@@ -5,7 +5,7 @@ int num_digits(int n)
     int numdigits = 0;
     if (n == 0)
     {
-        numdigits = 1;
+        return 1;
     }
     else
     {
@@ -21,10 +21,23 @@ int num_digits(int n)
 int main()
 {
     // compute number of digits in an int
+    /*
     int n;
     std::cin >> n;
     int numdigits = num_digits(n);
     std::cout << numdigits << '\n';
+    */
+
+    // compute 1/1^2 + ... + 1/10^2
+    double s = 0;
+    for (int i = 1; i <= 10; ++i)
+    {
+        double term = 1.0 / (i * i);
+        s += term;
+        std::cout << i << ' ' << term << ' ' << s << '\n';
+    }
+    std::cout << s << '\n';
+
     
     return 0;
 }
