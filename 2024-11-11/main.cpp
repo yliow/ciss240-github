178ebab8e19a5a8c686a987255d7eeec6ca71d0b
Yihsiang Liow <yliow@ccis.edu>
2024-11-11 14:43:52 -0500

    no msg

---
+++ 2024-11-11/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    // compute number of digits in an int
+    int n;
+    std::cin >> n;
+
+    int numdigits = 9;
+    while (n != 0)
+    {
+        std::cout << "\ntop ... n, numdigits:" << n << ' ' << numdigits << '\n';
+        n = n / 10;
+        ++numdigits;
+        std::cout << "bottom ... n, numdigits:" << n << ' ' << numdigits << '\n';
+    }
+
+    std::cout << numdigits << '\n';
+
+    return 0;
+}
