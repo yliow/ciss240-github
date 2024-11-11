3eaf3c1af5c0b368bc3e45bd3f6d3ae5223803d5
Yihsiang Liow <yliow@ccis.edu>
2024-11-11 14:56:43 -0500

    no msg

--- 2024-11-11/main.cpp
+++ 2024-11-11/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 
+int num_digits(int n)
+{
+    int numdigits = 0;
+    if (n == 0)
+    {
+        numdigits = 1;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            n /= 10;
+            ++numdigits;
+        }
+    }
+    return numdigits;
+}
+
 int main()
 {
     // compute number of digits in an int
     int n;
     std::cin >> n;
-
-    int numdigits = 0;
-    while (n != 0)
-    {
-        std::cout << "\ntop ... n, numdigits:" << n << ' ' << numdigits << '\n';
-        n = n / 10;
-        ++numdigits;
-        std::cout << "bottom ... n, numdigits:" << n << ' ' << numdigits << '\n';
-    }
-
+    int numdigits = num_digits(n);
     std::cout << numdigits << '\n';
-
+    
     return 0;
 }
