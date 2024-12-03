76f9358fbd2753702ce349378bbc9345654d4911
Yihsiang Liow <yliow@ccis.edu>
2024-12-03 15:03:19 -0500

    no msg

--- 2024-12-02/main.cpp
+++ 2024-12-02/main.cpp
@@ -2,6 +2,66 @@
 
 int square(int); // function prototype
 int cube(int);
+int sign(double);
+int sum(int);
+void printHelloWorld(int);
+void print42();
+
+int main()
+{
+    std::cout << square(3) << '\n';
+    int i = 5;
+    std::cout << square(i) << '\n';
+    square(10);
+    
+    std::cout << cube(2) << '\n';
+    std::cout << cube(10) << '\n';
+
+    std::cout << sign(-1.2) << '\n';
+    std::cout << sign(0.0) << '\n';
+    std::cout << sign(3.14) << '\n';
+
+    std::cout << sum(3) << '\n';
+    std::cout << sum(10) << '\n';
+    printHelloWorld(42);
+    print42();
+    return 0;
+}
+
+void print42()
+{
+    std::cout << "here's 42 for you ....\n";
+}
+
+void printHelloWorld(int x)
+{
+    std::cout << "hello world ... " << x << " received ...\n";
+}
+
+int sum(int n)
+{
+    int s = 0;
+    for (int i = 0; i <= n; ++i)
+    {
+        s += i;
+    }
+    return s;
+}
+
+
+int square(int x)
+{
+    // int ret = x * x;
+    // return ret;
+    return x * x;
+}
+
+
+int cube(int i)
+{
+    int ret = i * i * i;
+    return ret;
+}
 
 int sign(double x)
 {
@@ -20,36 +80,3 @@ int sign(double x)
     }
     return ret;
 }
-
-
-int main()
-{
-    std::cout << square(3) << '\n';
-    int i = 5;
-    std::cout << square(i) << '\n';
-
-    std::cout << cube(2) << '\n';
-    std::cout << cube(10) << '\n';
-
-    std::cout << sign(-1.2) << '\n';
-    std::cout << sign(0.0) << '\n';
-    std::cout << sign(3.14) << '\n';
-    return 0;
-}
-
-
-
-int square(int x)
-{
-    // int ret = x * x;
-    // return ret;
-    return x * x;
-}
-
-
-int cube(int i)
-{
-    int ret = i * i * i;
-    return ret;
-}
-
