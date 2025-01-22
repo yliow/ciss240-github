2d9a6294d2204065c2ecf9646f617af25bbecd57
Yihsiang Liow <yliow@ccis.edu>
2025-01-22 15:06:16 -0500

    no msg

---
+++ 2025-01-17/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 2 / 3 << ' ' << 2 % 3 << '\n';
+    std::cout << 17 / 4 << ' ' << 17 % 4 << '\n';
+
+
+    std::cout << 103 % 10 << '\n';
+    std::cout << 135246 % 10 << '\n';
+    std::cout << 13524 % 10 << '\n';
+
+    std::cout << 135246 % 100 << '\n';
+    std::cout << 135246 / 100 << '\n';
+
+    return 0;
+}
