fc76cdf88c1186705e49ef1b0b5f6fba1bcedb48
Yihsiang Liow <yliow@ccis.edu>
2025-02-19 14:52:54 -0500

    no msg

--- 2025-02-18/main.cpp
+++ 2025-02-18/main.cpp
@@ -24,109 +24,115 @@ int main()
         
     int q = KITCHEN; // player starts in the kitchen.
     char input;
-    switch (q)
-    {
-        case KITCHEN:
-        {
-            std::cout << "You are in the kitchen.\n"
-                      << "options: n, e\n";
-            std::cin >> input;
-            switch (input)
-            {
-                case 'n':
-                {
-                    q = LIBRARY;
-                    break;
-                }
-                case 'e':
-                {
-                    q = BEDROOM;
-                    break;
-                }
-                default:
-                {
-                    std::cout << "invalid input\n";
-                    break;
-                }
-            } // switch (input)
-            break;
-        }
-        case LIBRARY:
-        {
-            std::cout << "You are in the library.\n"
-                      << "options: e, w\n";
-            std::cin >> input;
-            switch (input)
-            {
-                case 'e':
-                {
-                    q = KITCHEN;
-                    break;
-                }
-                case 'w':
-                {
-                    q = STUDY;
-                    break;
-                }
-                default:
-                {
-                    std::cout << "invalid input\n";
-                    break;
-                }
-            }
 
-            break;
-        }
-        case STUDY:
+    for (int i = 0; i < 4; i = i + 1)
+    {
+        std::cout << "i: " << i << '\n';
+        switch (q)
         {
-            std::cout << "You are in the study.\n"
-                      << "options: n, w\n";
-            std::cin >> input;
-            switch (input)
+            case KITCHEN:
             {
-                case 'n':
+                std::cout << "You are in the kitchen.\n"
+                          << "options: n, e\n";
+                std::cin >> input;
+                switch (input)
                 {
-                    q = BEDROOM;
-                    break;
-                }
-                case 'w':
-                {
-                    q = LIBRARY;
-                    break;
-                }
-                default:
-                {
-                    std::cout << "invalid input\n";
-                }
+                    case 'n':
+                    {
+                        q = LIBRARY;
+                        break;
+                    }
+                    case 'e':
+                    {
+                        q = BEDROOM;
+                        break;
+                    }
+                    default:
+                    {
+                        std::cout << "invalid input\n";
+                        break;
+                    }
+                } // switch (input)
+                break;
             }
-            break;
-        }
-        case BEDROOM:
-        {
-            std::cout << "You are in the bedroom.\n"
-                      << "options: s, w\n";
-            std::cin >> input;
-            switch (input)
+            case LIBRARY:
             {
-                case 's':
+                std::cout << "You are in the library.\n"
+                          << "options: e, w\n";
+                std::cin >> input;
+                switch (input)
                 {
-                    q = STUDY;
-                    break;
+                    case 'e':
+                    {
+                        q = KITCHEN;
+                        break;
+                    }
+                    case 'w':
+                    {
+                        q = STUDY;
+                        break;
+                    }
+                    default:
+                    {
+                        std::cout << "invalid input\n";
+                        break;
+                    }
                 }
-                case 'w':
-                {
-                    q = KITCHEN;
-                    break;
-                }
-                default:
-                {
-                    std::cout << "invalid input\n";
-                    break;
-                }
+                
+                break;
             }
-            break;
-        }
-    }
-    
+            case STUDY:
+            {
+                std::cout << "You are in the study.\n"
+                          << "options: n, w\n";
+                std::cin >> input;
+                switch (input)
+                {
+                    case 'n':
+                    {
+                        q = BEDROOM;
+                        break;
+                    }
+                    case 'w':
+                    {
+                        q = LIBRARY;
+                        break;
+                    }
+                    default:
+                    {
+                        std::cout << "invalid input\n";
+                    }
+                }
+                break;
+            }
+            case BEDROOM:
+            {
+                std::cout << "You are in the bedroom.\n"
+                          << "options: s, w\n";
+                std::cin >> input;
+                switch (input)
+                {
+                    case 's':
+                    {
+                        q = STUDY;
+                        break;
+                    }
+                    case 'w':
+                    {
+                        q = KITCHEN;
+                        break;
+                    }
+                    default:
+                    {
+                        std::cout << "invalid input\n";
+                        break;
+                    }
+                }
+                break;
+            }
+        } // switch (q)
+    } // for
+
+    std::cout << "end of game loop\n";
     return 0;
 }
