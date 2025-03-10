941e73f54631cef8e64032719e5ebfafff46ce17
Yihsiang Liow <yliow@ccis.edu>
2025-03-10 14:31:20 -0400

    no msg

---
+++ 2025-03-07/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int i;
+    int sum = 0;
+
+    std::cout << "hi\n";
+    for (i = 0; i < 5; ++i)
+    {
+        std::cout << i << '\n';
+    }
+
+    std::cout << i << '\n';
+
+    return 0;
+}
