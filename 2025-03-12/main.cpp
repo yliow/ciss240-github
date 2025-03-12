315dd5bd1b381fdb54143ee43c83857cc45d188a
Yihsiang Liow <yliow@ccis.edu>
2025-03-12 15:23:42 -0400

    no msg

--- 2025-03-12/main.cpp
+++ 2025-03-12/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <cmath>
+
+bool isprime(int n)
+{
+    for (int d = 2; d <= sqrt(n); ++d)
+    {
+        if (n % d == 0)
+        {
+            return  false;   
+        }
+    }
+    return true;
+}
 
 int main()
 {
@@ -14,16 +27,60 @@ int main()
     // }
     // std::cout << "out of loop\n";
 
-    int n;
-    std::cin >> n;
+    // int n;
+    // std::cin >> n;
 
-    for (int i = 1; i <= n; ++i)
+    // if (n > 0)
+    // {
+    //     for (int i = 1; i <= n; ++i)
+    //     {
+    //         if (n % i == 0)
+    //         {
+    //             std::cout << i << '\n';
+    //         }
+    //     }
+    // }
+    // else
+    // {
+    //     std::cout << "Wrong input. Run again and enter a positive int.\n";
+    // }
+
+    // int n;
+    // std::cin >> n;
+
+    // bool isprime = true;
+    // for (int d = 2; d < n; ++d)
+    // {
+    //     if (n % d == 0)
+    //     {
+    //         isprime = false;
+    //         break;
+    //     }
+    // }
+
+    // std::cout << (isprime ? "prime\n" : "not prime\n");
+    
+    // for (int n = 2; n <= 1000000; ++n)
+    // {
+    //     if (isprime(n))
+    //     {
+    //         std::cout << n << '\n';
+    //     }
+    // }
+
+    for (int x = 0; x <= 20; ++x)
     {
-        if (n % i == 0)
+        for (int y = 0; y <= 20; ++y)
         {
-            std::cout << i << '\n';
+            for (int z = 0; z <= 20; ++z)
+            {
+                if (x * x + y * y == z)
+                {
+                    std::cout << x << ' ' << y << ' ' << z << '\n';
+                }
+            }
         }
     }
-    
+
     return 0;
 }
