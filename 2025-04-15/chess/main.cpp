483ac8bd8a38ea8404d6deb1b454f18be782b73f
Yihsiang Liow <yliow@ccis.edu>
2025-04-15 15:39:01 -0400

    no msg

---
+++ 2025-04-15/chess/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    // char board[2][3] = {{'c', 'a', 't'},
+    //                     {'c', 'a', 'b'}};
+
+    // for (int r = 0; r < 2; ++r)
+    // {
+    //     std::cout << "+-+-+-+\n";
+    //     std::cout << '|';
+    //     for (int c = 0; c < 3; ++c)
+    //     {
+    //         std::cout << board[r][c] << '|';
+    //     }
+    //     std::cout << '\n';
+    // }
+    // std::cout << "+-+-+-+\n";
+   
+    return 0;
+}
