114e63ae3773cc737da2583e2ca8120f057e7419
Yihsiang Liow <yliow@ccis.edu>
2025-09-10 14:37:06 -0400

    no msg

---
+++ 2025-09-08/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    std::cin >> x;
+    std::cout << "x: " << x << '\n';
+    x = 42;
+    std::cout << "x: " << x << '\n';
+    
+    return 0;
+}
