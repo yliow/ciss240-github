479cc3f365faa658646a3c15655c0a2e3fe3d6c5
Yihsiang Liow <yliow@ccis.edu>
2025-09-12 14:49:00 -0400

    no msg

---
+++ 2025-09-10/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // int x, y, z;
+    // std::cin >> x;
+    // std::cin >> y;
+    // std::cin >> z;
+
+    // int sum = x + y + z;
+    // std::cout << sum << '\n';
+
+
+    std::cout << "1358762 / 1000 = "
+              << 1358762 / 1000 << '\n';
+    
+    return 0;
+}
