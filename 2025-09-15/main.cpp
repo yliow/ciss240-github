0ca7753196e2cb054521b1ad685813ca3641dfe5
Yihsiang Liow <yliow@ccis.edu>
2025-09-15 14:52:45 -0400

    no msg

---
+++ 2025-09-15/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Multiplicator !!!\n";
+    std::cout << "Enter an integer: ";
+    int x;
+    std::cin >> x;
+    std::cout << "Enter another integer: ";
+    int y;
+    std::cin >> y;
+
+    
+    return 0;
+}
