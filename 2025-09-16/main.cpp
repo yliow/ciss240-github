29c49b141fec64727ac221b0def00bbe8539fb2e
Yihsiang Liow <yliow@ccis.edu>
2025-09-17 14:39:20 -0400

    no msg

---
+++ 2025-09-16/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 1.2345 << '\n';
+    std::cout << 1 + 2 << '\n';
+    std::cout << 1.1 + 2.2 << '\n';
+
+    std::cout << 1.2345f << '\n';
+
+    return 0;
+}
