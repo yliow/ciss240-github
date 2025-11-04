9c6d9caef6ca4902f44b3668ea62b7e0e5618229
Yihsiang Liow <yliow@ccis.edu>
2025-11-04 12:16:45 -0800

    no msg

---
+++ 2025-11-03/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 3; i < 6; ++i)
+    {
+        std::cout << i << '\n';
+        for (int j = 0; j <= 2 * i; ++j)
+        {
+            std::cout << ' ' << j;
+        }
+        std::cout << '\n';
+    }
+    
+    return 0;
+}
