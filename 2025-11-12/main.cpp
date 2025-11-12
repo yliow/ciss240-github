a3dc918944b4b58e6e55715ea881f06d83852cf5
Yihsiang Liow <yliow@ccis.edu>
2025-11-12 12:10:48 -0800

    no msg

--- 2025-11-12/main.cpp
+++ 2025-11-12/main.cpp
@@ -28,16 +28,42 @@ int numdigits(int n)
     return numdigits_;
 }
 
+bool isprime(int n)
+{
+    bool isprime_ = true;
+    for (int d = 2; d <= n - 1; ++d)
+    {
+        if (n % d == 0)
+        {
+            isprime_ = false;
+            break;
+        }
+    }
+    return isprime_;
+}
+
 int main()
 {
     // int z = add_one(5);
     // std::cout << "z:" << z << '\n';
     // return 0;
     
+    // int n;
+    // std::cin >> n;
+    // int m = numdigits(n);
+    // std::cout << m << '\n';
+
     int n;
     std::cin >> n;
-    int m = numdigits(n);
-    std::cout << m << '\n';
+    bool m = isprime(n);
+    if (m)
+    {
+        std::cout << n << " is prime\n";
+    }
+    else
+    {
+        std::cout << n << " is not prime\n";
+    }
 
     return 0;
 }
