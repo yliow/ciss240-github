8f5be9cbe2f62d422aac61bfd7ff032b1c856d0a
Yihsiang Liow <yliow@ccis.edu>
2025-12-03 11:35:56 -0800

    no msg

--- 2025-12-03/chess/main.cpp
+++ 2025-12-03/chess/main.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
 
-void init(char board[8][8])
-{
-    for (int r = 0; r < 8; ++r)
-    {
-        for (int c = 0; c < 8; ++c)
-        {
-            board[r][c] = ' ';
-        }
-    }
+void init(char board[8][8]);
+void print(char board[8][8]);
 
-    char pieces[9] = "rnbqkbnr";
-    for (int c = 0; c < 8; ++c)
-    {
-        board[0][c] = pieces[c];
-        board[7][c] = pieces[c] - 'a' + 'A';
-    }
-    
-    for (int c = 0; c < 8; ++c)
-    {
-        board[1][c] = 'p';
-        board[6][c] = 'p';
-    }
-
-    return;
-}
 
 int main()
 {
     char board[8][8];
     init(board);
-   
-    // print the chessboard
+    print(board);
+
+    return 0;
+}
+
+
+// print the chessboard    
+void print(char board[8][8])
+{
     std::cout << "+-+-+-+-+-+-+-+-+\n";
     for (int r = 0; r < 8; ++r)
     {
@@ -44,5 +29,25 @@ int main()
         std::cout << "+-+-+-+-+-+-+-+-+\n";
     }
 
-    return 0;
+}
+
+void init(char board[8][8])
+{
+    for (int r = 2; r < 6; ++r)
+    {
+        for (int c = 0; c < 8; ++c)
+        {
+            board[r][c] = ' ';
+        }
+    }
+
+    char pieces[9] = "rnbqkbnr";
+    for (int c = 0; c < 8; ++c)
+    {
+        board[0][c] = pieces[c];
+        board[7][c] = pieces[c] - 'a' + 'A';
+        board[1][c] = 'p';
+        board[6][c] = 'p';
+    }
+    return;
 }
