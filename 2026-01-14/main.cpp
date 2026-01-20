ef6ef825e00d152c86d076e95f694073dcae4a28
Yihsiang Liow <yliow@ccis.edu>
2026-01-20 12:31:37 -0800

    no msg

---
+++ 2026-01-14/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world\n"
+              << "hi\tmom\n"
+              << "goodbye!\b\b\b\b\b\b\b\b\b\bhello!!!!\n";
+    return 0;
+}
+
