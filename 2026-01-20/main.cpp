70b5e81fc9660de6f31b163ee4e949a17509e8db
Yihsiang Liow <yliow@ccis.edu>
2026-01-20 12:47:07 -0800

    no msg

--- 2026-01-20/main.cpp
+++ 2026-01-20/main.cpp
@@ -2,6 +2,21 @@
 
 int main()
 {
-    std::cout << "?\n";
+    // std::cout << 12 / 3 << '\n';
+    // std::cout << 11 / 3 << '\n';
+    // std::cout << 10 / 3 << '\n';
+    // std::cout << 9 / 3 << '\n';
+    // std::cout << 8 / 3 << '\n';
+
+    // std::cout << 1 / 0 << '\n';
+
+    // std::cout << 43 / 8 << '\n';
+    // std::cout << 43 % 8 << '\n';
+
+    int minutes = 135;
+    std::cout << minutes << " minutes = ";
+    std::cout << minutes / 60 << " hours and ";
+    std::cout << minutes % 60 << " minutes\n";
+    
     return 0;
 }
