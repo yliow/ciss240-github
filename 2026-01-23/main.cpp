4ba7ed11604a62457b560aef20fe34a1f2bf5f85
Yihsiang Liow <yliow@ccis.edu>
2026-01-27 12:24:05 -0800

    no msg

---
+++ 2026-01-23/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+//    std::cout << x << '\n';
+    // int x = 42;
+    // int x = -1;
+
+    // int num_arms = 3;
+    // std::cout << "I have " << num_arms << " arms.\n";
+
+    // int x = 42, y;
+    // std::cout << x << ' ' << y << '\n';
+
+    // int x = 42;
+    // x = x + 1;
+    // std::cout << x << '\n';
+
+    int x = 0;
+    std::cout << "Enter integer x: ";
+    std::cin >> x;
+
+    int y = 0;
+    std::cout << "Enter integer y: ";
+    std::cin >> y;
+
+    std::cout << "x + y = " << x + y << '\n';
+    
+    return 0;
+}
