7c6d58a5ae8f151c48cb6eb46f5a456d3bdb7dfd
Yihsiang Liow <yliow@ccis.edu>
2026-01-28 11:28:44 -0800

    no msg

---
+++ 2026-01-27/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    // int x = 1, y = -6;
+    // std::cout << x << ", " << y << '\n';
+
+    // int t = x;
+    // x = y;
+    // y = t;
+    // std::cout << x << ", " << y << '\n';
+
+    // int x = 2147483647;
+    // std::cout << x << '\n';
+    // x = x + 1;
+    // std::cout << x << '\n';
+
+    // int x = 42;
+    // std::cout << "1: ["
+    //           << std::setw(5) << std::setfill('*') << x << "]\n"
+    //           << "2: [" << std::setw(20) << "abc" << "]\n";
+
+    // std::cout << 1.2345 << '\n'
+    //           << 1e-1 << '\n'
+    //           << 1e2 << '\n'
+    //           << 1.2E+3f  << '\n';
+
+    // double x;
+    // std::cin >> x;
+    // std::cout << x << ' ' << x + 0.5
+    //           << '\n';
+
+    //std::cout << 1.1 % 2.2 << '\n';
+
+    std::cout << '[' << std::right
+              << std::setw(3) << 1
+              << ']'
+              << '\n';
+    return 0;
+}
