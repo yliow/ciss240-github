89f4a8ec33e830a094adcef8a32841c04ff6fadd
Yihsiang Liow <yliow@ccis.edu>
2026-02-03 12:03:55 -0800

    no msg

---
+++ 2026-01-30/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main()
+{
+    // const double PI = 3.14159;
+    // std::cout << PI << '\n';
+    // PI = 3;
+    // std::cout << PI << '\n';
+
+    // const int PAID_MTHS_PER_YR = 11;
+    // int monthly_salary;
+    // std::cout << "Ente salary per month: ";
+    // std::cin >> monthly_salary;
+    // std::cout << "In " << PAID_MTHS_PER_YR
+    //           << months the employee earns $"
+    //           << monthly_salary * PAID_MTHS_PER_YR
+    //           << '\n';
+
+    // std::cout << true << ' ' << false << '\n';
+
+    // std::cout << "true && true  = " << (true && true) << '\n'
+    //           << "true && false = " << (true && false) << '\n'
+    //           << "false && true = " << (false && true) << '\n'
+    //           << "false && false = " << (false && false) << '\n';
+
+    // std::cout << "true || true  = " << (true || true) << '\n'
+    //           << "true || false = " << (true || false) << '\n'
+    //           << "false || true = " << (false || true) << '\n'
+    //           << "false || false = " << (false || false) << '\n';
+    // std::cout << "!true  = " << (!true) << '\n'
+    //           << "!false = " << (!false) << '\n';
+
+    // std::cout << (-3 <= -2 <= -1) << '\n';
+    // std::cout << ((-3 <= -2) && (-2 <= -1)) << '\n';
+
+    // double x;
+    // std::cin >> x;
+    // bool in_left_range = (x >= -15 && x <= -5);
+    // //std::cout << in_left_range << '\n';
+    // bool in_right_range = (x >= 5 && x <= 15); 
+    // //std::cout << in_right_range << '\n';
+    // bool in_range = (in_left_range || in_right_range);
+    // std::cout << in_range << '\n';
+
+    
+    
+    return 0;
+}
