01f7ce998f93adee285cb1fd3c10b6a3c8d8343a
Yihsiang Liow <yliow@ccis.edu>
2026-02-06 11:35:10 -0800

    no msg

---
+++ 2026-02-06/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    // int a = 0;
+    // std::cin >> a;
+
+    // if (a == 42)
+    // {
+    //     std::cout << "C++: Mine too!\n";
+    // }
+    // else
+    // {
+    //     std::cout << "C++: boo!\n";
+    // }
+
+    // int i;
+    // std::cin i >> i;
+    // if (i % 2 == 0)
+    // {
+    //     std::cout << "even!!!\n";
+    // }
+    // else
+    // {
+    //     std::cout << "odd!!!\n";
+    // }
+
+
+    return 0;
+}
