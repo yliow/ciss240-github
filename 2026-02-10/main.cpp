b920b705deb8411e430459a49c086575151a34ab
Yihsiang Liow <yliow@ccis.edu>
2026-02-11 11:39:01 -0800

    no msg

---
+++ 2026-02-10/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    // int x = 0;
+    // std::cin >> x;
+
+    // switch (x)
+    // {
+    //     case 1:
+    //         std::cout << "yo\n";
+    //         break;
+    //     case 2:
+    //     case 3:
+    //         std::cout << "oh?\n";
+    //         break;
+    //     default:
+    //         std::cout << "huh?\n";
+    // }
+    // std::cout << "out of switch\n";
+
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << i << '\n';
+    }
+    
+    return 0;
+}
