276555a6f7c36316946623479a98af583284afca
Yihsiang Liow <yliow@ccis.edu>
2026-09-18 11:26:29 -0700

    no msg

---
+++ 2026-09-16/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+    // int x;
+    // std::cin >> x;
+    // std::cout << "x: " << x << '\n';
+    // x = 42;
+    // std::cout << "x: " << x << '\n';
+
+    // // program gets input as number of minutes
+    // // and print the time but in hours and minutes
+    // std::cin >> minutes;
+    // int minutes; 
+    
+    // int hh; 
+    // int mm; 
+    // hh = minutes / 60;
+    // mm = minutes % 60;
+
+    // std::cout << hh << ' ' << mm << '\n';
+
+    int i, j;
+    std::cin >> i >> j;
+    int product = i * j;
+    std::cout << product << '\n';
+    
+    return 0;
+}
